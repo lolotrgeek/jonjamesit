a83b6c43a470467f3682a65c6a93857b25c824aa
lolotrgeek <lolotrgeek@gmail.com>
2020-07-31 17:16:55 -0500

    update about

--- public/pages/about.js
+++ public/pages/about.js
@@ -1,35 +1,33 @@
 let about = `
 <h1>About</h1>
 <h2>Life Mission</h2>
-<p>Raise the standard of living for all humanity.
-    <br>Food, water, and shelter for all people.
-    <br>Make us more adapted to any environment.
-    <br>Free humanity to spend our time and energy solving a higher order set of problems.
-    <br>Ultimately, creating a platform so together we can reveal the nature of reality.
-</p>
+<p>Raise the standard of living and free humanity to spend time and energy solving problems beyond survival.</p>
 
 <h2>Goals</h2>
 <ul>
-    <li>Eliminate labor through automation.</li>
-    <li>Empower adaptation.</li>
+    <li>Eliminate labor.</li>
     <li>Create abundance.</li>
-    <li>Usher in a post-monetary world.</li>
+    <li>Solve survival.</li>
 </ul>
 
 <h2>Theory</h2>
 <ul>
     <li>evolution</li>
-    <li>quantum mechanics</li>
     <li>sustainablity</li>
     <li>efficiency</li>
     <li>abundance</li>
 </ul>
 
-<h2>Practice</h2>
-<ul>
-    <li>Entrepreneurship</li>
-    <li>reinforcement learning</li>
-    <li>renewable energy sources</li>
-    <li>neuromorphic hardware</li>
-</ul>
+<h2>Hypothesis</h2>
+<p>Is it possible to create a balanced distribution of resources? 
+Either the <a href="https://en.wikipedia.org/wiki/Pareto_distribution">Pareto distribution</a> is an inherit feature of reality or a human construct. 
+If it's a human problem, then it is solved over time with evolution. 
+If it is a feature of reality then it is solved by breaking and reforming reality.
+To test this we need to remove the incentives that cause imbalanced resource distributions. 
+The primary incentives are the time and energy required to attain and sustain life.
+Therefore when the time and energy problem is solved then we can test the distribution problem. 
+
+The solution to the time and energy problem right now seems to be automation.
+I am dedicating my time and energy to realizing automation which seems to be the best solution to achieving a sustainable <a href="https://www.jimcollins.com/concepts/the-flywheel.html">fly wheel effect</a> that can persist.
+</p> 
 `
